fb3e6e26d0607c762406c16caf9bc9ea0969517d
chester <chestercen@tenvine.cn>
2021-06-16 19:33:01 +0800

    feat(log): DisableQuote: true,

--- log/log.go
+++ log/log.go
@@ -179,6 +179,7 @@ func setLogFormatter() {
 	std.SetFormatter(&logrus.TextFormatter{
 		TimestampFormat:  FormatTimestamp,
 		CallerPrettyfier: callerPrettyfier,
+		DisableQuote:     true,
 	},
 	)
 }
