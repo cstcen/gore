cebd479d7f94ce422518b8fc36a309007703c3c1
chester <chestercen@tenvine.cn>
2021-06-21 14:32:51 +0800

    feat(http): add http.go

---
+++ http.go
@@ -0,0 +1,79 @@
+package gore
+
+import (
+	"bytes"
+	"git.tenvine.cn/backend/gore/log"
+	"git.tenvine.cn/backend/gore/model"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+	"time"
+)
+
+var DefaultHttpClient = &http.Client{
+	Transport: &Transport{
+		RoundTripper: http.DefaultTransport,
+	},
+	Timeout: TimeoutConn,
+}
+
+type ClientIPGetter interface {
+	ClientIP() string
+}
+
+type Transport struct {
+	http.RoundTripper
+}
+
+func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	ctx, ok := req.Context().(*gin.Context)
+	if !ok {
+		return nil, model.BaseResult{Code: http.StatusInternalServerError, Message: "context is not gin.context"}
+	}
+
+	contextLog := log.WithContext(ctx)
+
+	// Start timer
+	start := time.Now()
+	header := req.Header
+	path := req.URL.Path
+	raw := req.URL.RawQuery
+	if raw != "" {
+		path = path + "?" + raw
+	}
+	var reqBuf bytes.Buffer
+	reqTee := io.TeeReader(req.Body, &reqBuf)
+	reqBody, _ := io.ReadAll(reqTee)
+	req.Body = io.NopCloser(&reqBuf)
+
+	contextLog.Infof("HTTPClient url:    %s", path)
+	contextLog.Infof("HTTPClient header: %+v", header)
+	contextLog.Infof("HTTPClient body:   %+v", string(reqBody))
+
+	resp, err := t.RoundTripper.RoundTrip(req)
+	if err != nil {
+		return resp, err
+	}
+
+	var respBuf bytes.Buffer
+	respTee := io.TeeReader(resp.Body, &respBuf)
+	respBody, _ := io.ReadAll(respTee)
+	resp.Body = io.NopCloser(&respBuf)
+
+	// Stop timer
+	latency := time.Now().Sub(start)
+	method := req.Method
+	statusCode := resp.StatusCode
+
+	contextLog.WithFields(logrus.Fields{
+		"method":  method,
+		"uri":     path,
+		"status":  statusCode,
+		"latency": latency,
+		"ip":      req.RemoteAddr,
+		"body":    string(respBody),
+	}).Info("HTTPClient")
+
+	return resp, nil
+}
