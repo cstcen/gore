20f2b4f82e09f2dd3999d8deafb045eaea276a23
chester <chestercen@tenvine.cn>
2021-08-24 17:15:27 +0800

    feat(http): log

--- http/http.go
+++ http/http.go
@@ -5,12 +5,8 @@ import (
 	"context"
 	"encoding/json"
 	"git.tenvine.cn/backend/gore/constant"
-	"git.tenvine.cn/backend/gore/log"
-	"git.tenvine.cn/backend/gore/vo"
-	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
-	"time"
 )
 
 var cli *http.Client
@@ -134,64 +130,4 @@ func RespHandler(resp *http.Response, expectedPtr interface{}) error {
 		return err
 	}
 	return nil
-}
-
-type Transport struct {
-	http.RoundTripper
-}
-
-func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
-	ctx, ok := req.Context().(context.Context)
-	if !ok {
-		return nil, vo.BaseResult{Code: http.StatusInternalServerError, Message: "unknown context"}
-	}
-
-	contextLog := log.WithContext(ctx)
-
-	// Start timer
-	start := time.Now()
-	header := req.Header
-	path := req.URL.Path
-	raw := req.URL.RawQuery
-	if len(raw) > 0 {
-		path = path + "?" + raw
-	}
-	var reqBuf bytes.Buffer
-	var reqBody []byte
-	var err error
-	if req.Body != nil {
-		reqTee := io.TeeReader(req.Body, &reqBuf)
-		reqBody, _ = io.ReadAll(reqTee)
-		req.Body = io.NopCloser(&reqBuf)
-	}
-
-	contextLog.Tracef("HTTPClient url:    %s", req.URL.String())
-	contextLog.Tracef("HTTPClient header: %+v", header)
-	contextLog.Tracef("HTTPClient body:   %+v", string(reqBody))
-
-	resp, err := t.RoundTripper.RoundTrip(req)
-	if err != nil {
-		return resp, err
-	}
-
-	var respBuf bytes.Buffer
-	respTee := io.TeeReader(resp.Body, &respBuf)
-	respBody, _ := io.ReadAll(respTee)
-	resp.Body = io.NopCloser(&respBuf)
-
-	// Stop timer
-	latency := time.Now().Sub(start)
-	method := req.Method
-	statusCode := resp.StatusCode
-
-	contextLog.WithFields(logrus.Fields{
-		"method":  method,
-		"uri":     path,
-		"status":  statusCode,
-		"latency": latency,
-		"ip":      req.RemoteAddr,
-		"body":    string(respBody),
-	}).Info("HTTPClient resp: ")
-
-	return resp, nil
 }
